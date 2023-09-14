3798443c4ad46847d500ca5712d13fce7026b2fa
ngsowjanya <98144148+ngsowjanya@users.noreply.github.com>
2023-09-14 17:53:29 +0530

    Add files via upload

---
+++ CgOnlineShopSpringBootRestMvcOrmData/src/main/java/com/cg/onlineshop/CgWalletSpringBootRestMvcOrmData1Application.java
@@ -0,0 +1,13 @@
+package com.cg.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgWalletSpringBootRestMvcOrmData1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgWalletSpringBootRestMvcOrmData1Application.class, args);
+	}
+
+}
---
+++ CgOnlineShopSpringBootRestMvcOrmData/src/test/java/com/cg/onlineshop/CgWalletSpringBootRestMvcOrmData1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgWalletSpringBootRestMvcOrmData1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
